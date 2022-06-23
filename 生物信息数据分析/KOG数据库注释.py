9a4eba1c81600b8f7967b304915c84d1c7010d0f
peiy <695819143@qq.com>
2022-06-23 21:22:56 +0800

    Default Changelist

--- 生物信息数据分析/KOG数据库注释.py
+++ 生物信息数据分析/KOG数据库注释.py
@@ -1,11 +1,11 @@
 import pandas as pd
-path = "E:\售后\吴春霞\kog_1000.txt"
+
+path = "E:\售后\李淑霞\\result.txt"
 with open(path, 'r') as file_handle:
     ls = []
     for line in file_handle.readlines():
-        if line.strip('\n').startswith("TRINITY"):
-            new_line = line.strip('\n').split('\t')
-            ls.append(new_line)
+        new_line = line.strip('\n').split('\t')
+        ls.append(new_line)
 
     df = pd.DataFrame(ls, columns=["query acc.ver", "subject acc.ver",
                                          "% identity", "alignment length",
@@ -13,4 +13,10 @@ with open(path, 'r') as file_handle:
                                          "q. start", "q. end", "s. start",
                                          "s. end", "evalue", "bit score"])
     df2 = df.drop_duplicates('query acc.ver')
-    print(df2)
+    df2.to_excel("E:\售后\李淑霞\\result.xlsx" )
+
+
+
+
+
+
