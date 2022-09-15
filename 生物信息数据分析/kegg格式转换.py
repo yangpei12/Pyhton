35712e42c477c412fdd1baf5d4d9f160c228d27b
yangpei12 <50899817+yangpei12@users.noreply.github.com>
2022-09-15 21:46:27 +0800

    Update kegg格式转换.py

--- 生物信息数据分析/kegg格式转换.py
+++ 生物信息数据分析/kegg格式转换.py
@@ -15,6 +15,26 @@ def keggfun(id_nummber):
     output_handle = pd.Series(dicts)
     return output_handle
 
+
+
+# 使用apply, 不能运行成功
+unique_id = set(Input_data['gene_id'])
+gene_id = pd.DataFrame(list(unique_id))
+top10 = gene_id.head(10)
+result = top10.apply(keggfun)
+print(result)
+
+"""
+# 使用map，map只能用于series数据结构中
+unique_id = set(Input_data['gene_id'])
+gene_id = pd.Series(list(unique_id))
+top10 = gene_id.head(10)
+result = top10.map(keggfun)
+print(result)
+
+# 使用for循环
 for temp in Input_data['gene_id']:
     result = keggfun(temp)
     result.to_csv('Output/output_kegg背景.csv', mode='a', header=False)
+"""
+
